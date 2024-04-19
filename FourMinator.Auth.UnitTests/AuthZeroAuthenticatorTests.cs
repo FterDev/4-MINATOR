e00684e33eadc1615102bf4d217ef4c27eb66ac0
Fedor Terekhov <devops@terekhov.ch>
2024-04-19 18:09:16 +0200

    Added AuthZeroAuth Test class

---
+++ FourMinator.Auth.UnitTests/AuthZeroAuthenticatorTests.cs
@@ -0,0 +1,11 @@
+using Xunit;
+using Moq;
+using FluentAssertions;
+
+namespace FourMinator.Auth.UnitTests
+{
+    public class AuthZeroAuthenticatorTests
+    {
+        // Add your test methods here
+    }
+}
