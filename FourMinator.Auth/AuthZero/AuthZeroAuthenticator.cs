bf7817f584efcb74c339f567651ed954f335ca9c
Fedor Terekhov <devops@terekhov.ch>
2024-04-22 14:54:22 +0200

    Added AuthKey Propset

--- FourMinator.Auth/AuthZero/AuthZeroAuthenticator.cs
+++ FourMinator.Auth/AuthZero/AuthZeroAuthenticator.cs
@@ -19,12 +19,14 @@ namespace Fourminator.Auth
         {
             byte[] authKeyBytes = Convert.FromBase64String(authKeyBase64);
             string authKey = Encoding.UTF8.GetString(authKeyBytes);
+            this.AuthKey = authKey;
             return authKey;
         }
 
         public string GenerateAuthKey()
         {
             var authKey = RandomString.GetString(Types.ALPHANUMERIC_MIXEDCASE , 64);
+            this.AuthKey = authKey;
             return authKey;
         }
 
