c5a8199bdd70b05d79b123444d12af3b0ca20c8d
Fedor Terekhov <fedor@terekhov.ch>
2024-04-23 22:18:20 +0200

    Added IdentityProvider Repository Contract

---
+++ FourMinator.Auth/Persistence/Repository/IIdentityProviderRepository.cs
@@ -0,0 +1,16 @@
+﻿using Fourminator.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourMinator.Auth.Persistence.Repository
+{
+    public interface IIdentityProviderRepository
+    {
+        Task<IdentityProvider> CreateIdentityProvider(IdentityProvider identityProvider);
+        Task<IdentityProvider> GetIdentityProviderByKey(string key);
+
+    }
+}
