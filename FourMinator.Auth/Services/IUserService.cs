72050ed7c352b53030761c47aeb2f552e6e54edd
Fedor Terekhov <fedor@terekhov.ch>
2024-04-28 14:40:34 +0200

    Created UserService Interface

---
+++ FourMinator.Auth/Services/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using FourMinator.Auth;
+
+
+namespace FourMinator
+{
+    internal interface IUserService
+    {
+        Task<User?> GetUserByEmail(string email);
+        Task<bool> CreateUser(string nickname, string email);
+    }
+}
