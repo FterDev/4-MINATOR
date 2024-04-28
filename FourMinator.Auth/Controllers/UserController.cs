bf687798d67c66c1954eded5f6c28e299099fd30
Fedor Terekhov <fedor@terekhov.ch>
2024-04-28 14:54:37 +0200

    Added UserController

---
+++ FourMinator.Auth/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace FourMinator.Auth
+{
+    public class UserController
+    {
+        private IUserRepository _userRepository;
+        
+        public UserController(DbContext context)
+        {
+            _userRepository = new UserRepository(context);
+        }
+
+
+
+
+
+    }
+}
