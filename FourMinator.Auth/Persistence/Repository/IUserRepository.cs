06377d35a6bdd5a55e4dbfbaaf857f0612e4d7df
Fedor Terekhov <fedor@terekhov.ch>
2024-04-28 14:44:28 +0200

    Added User Repository

---
+++ FourMinator.Auth/Persistence/Repository/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourMinator.Auth
+{
+    internal interface IUserRepository
+    {
+        Task<User?> GetUserByEmail(string email);
+        Task<bool> CreateUser(string nickname, string email);
+    }
+}
---
+++ FourMinator.Auth/Persistence/Repository/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace FourMinator.Auth
+{
+    internal class UserRepository : IUserRepository
+    {
+        private readonly DbContext _context;
+        public UserRepository(DbContext context) 
+        {
+            _context = context;    
+        }
+        public Task<bool> CreateUser(string nickname, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetUserByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+  
+}
