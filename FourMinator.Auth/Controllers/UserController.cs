3bd6cb2178f549a5a60fface057d68f440d247a1
Fedor Terekhov <fedor@terekhov.ch>
2024-04-28 15:14:08 +0200

    Implemented UserController

--- FourMinator.Auth/Controllers/UserController.cs
+++ FourMinator.Auth/Controllers/UserController.cs
@@ -1,22 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Http;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+
 
 namespace FourMinator.Auth
 {
+    [Route("api/[controller]")]
     public class UserController
     {
         private IUserRepository _userRepository;
+        private IIdentityProviderAuthenticator _identityProviderAuthenticator;
         
-        public UserController(DbContext context)
+        public UserController(DbContext context, IIdentityProviderAuthenticator ipAuth)
         {
             _userRepository = new UserRepository(context);
+            _identityProviderAuthenticator = ipAuth;
         }
 
 
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromHeader(Name = "Auth-Key")] string authKey, [FromBody] User user)
+        {
+            if(_identityProviderAuthenticator.ValidateAuthKey(authKey))
+            {
+                var userCheck = await _userRepository.GetUserByEmail(user.Email);
+                if(userCheck == null)
+                {
+                    await _userRepository.CreateUser(user.Nickname, user.Email);
+                    return new OkResult();
+                }
+                else
+                {
+                    return new BadRequestObjectResult("User already exists");
+                }
+            }
+            else
+            {
+                return new UnauthorizedResult();
+            }
+        }
 
 
 
