6a9822e752677224dbda9e21a27c1f63c3bb331f
Fedor Terekhov <fedor@terekhov.ch>
2024-04-28 14:27:56 +0200

    Added User Entity

---
+++ FourMinator.Auth/Domain/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourMinator.Auth
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Nickname { get; set; }
+        public string Email { get; set; }
+    }
+}
