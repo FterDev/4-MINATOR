5fb9ea5b2da03bc54ecb421b7745dad5a18c9d42
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-05-10 22:54:49 +0200

    Moved Context to Persistence

--- Fourminator.Persistence/FourminatorContext.cs
+++ Fourminator.Persistence/FourminatorContext.cs
@@ -1,9 +1,33 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FourMinator.Auth.Persistence.Domain;
+using FourMinator.Persistence.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 
 namespace Fourminator.Persistence
 {
     internal class FourminatorContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.secret.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("AuthContext");
+            var serverVersion = MySqlServerVersion.AutoDetect(connectionString);
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+        }
+
+
+        public DbSet<IdentityProvider> IdentityProviders { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<IdentityProvider>().HasKey(x => x.IdentityProviderId);
+            modelBuilder.Entity<User>().HasKey(x => x.Id);
+        }
     }
 }
