064cfa72dfffc87bfd752b1cf4c61cccc234e0ea
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-05-11 13:19:24 +0200

    Created first validator rules

---
+++ FourMinator.Robot/Services/RobotValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using FourMinator.Persistence.Domain;
+
+
+namespace FourMinator.RobotService.Services
+{
+    internal class RobotValidator : AbstractValidator<Robot>
+    {
+        public RobotValidator()
+        {
+            RuleFor(r => r.Name).NotEmpty().WithMessage("Name cannot be empty");
+            RuleFor(r => r.Name).MaximumLength(50).WithMessage("Name cannot be longer than 50 characters");
+        }
+    }
+}
