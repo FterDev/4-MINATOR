b27035da1b1a5ac7b7bf6854932c55b8ee91f62d
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-05-11 12:55:06 +0200

    Added empty RobotRepo

---
+++ FourMinator.Robot/Persistence/RobotRepository.cs
@@ -0,0 +1,34 @@
+﻿
+
+using FourMinator.Persistence.Domain;
+
+namespace FourMinator.RobotService
+{
+    internal class RobotRepository : IRobotRepository
+    {
+        public Task CreateRobot(Robot robot)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteRobot(Robot robot)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Robot>> GetAllRobots()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Robot?> GetRobotById(uint id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateRobot(Robot robot)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
