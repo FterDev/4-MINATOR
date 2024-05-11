fb8fe94aa9bcb711270343cf8794bba8dae3d3a5
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-05-11 10:29:48 +0200

    Robot set nullables

--- Fourminator.Persistence/Domain/Robot/Robot.cs
+++ Fourminator.Persistence/Domain/Robot/Robot.cs
@@ -8,9 +8,9 @@ namespace FourMinator.Persistence.Domain
         public uint Id { get; set; }
         public string Name { get; set; }
         public User CreatedBy { get; set; }
-        public string Password { get; set; }
-        public string Thumbprint { get; set; }
-        public string PublicKey { get; set; }
+        public string? Password { get; set; }
+        public string? Thumbprint { get; set; }
+        public string? PublicKey { get; set; }
         public Int16 Status { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
