7bbf70c8f77b99c8f0e4c020bb9ff8c08079f5b0
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-05-11 10:59:59 +0200

    Fixed Namespaces

--- FourMinator.Auth/Persistence/Repository/IUserRepository.cs
+++ FourMinator.Auth/Persistence/Repository/IUserRepository.cs
@@ -1,9 +1,4 @@
-﻿using FourMinator.Auth.Persistence.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FourMinator.Persistence.Domain;
 
 namespace FourMinator.Auth
 {
