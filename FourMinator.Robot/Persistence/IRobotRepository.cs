1bb7f6cba84a86ea96a495245b318b32cc1b6884
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-05-11 12:42:37 +0200

    Added Robot Repo Interface

---
+++ FourMinator.Robot/Persistence/IRobotRepository.cs
@@ -0,0 +1,17 @@
+﻿using FourMinator.Persistence.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourMinator.RobotService
+{
+    public interface IRobotRepository
+    {
+        Task CreateRobot(string name, string password, string thumbprint, string publicKey, User createdBy);
+        Task<Robot?> GetRobotById(uint id);
+        Task UpdateRobot(Robot robot);
+        Task DeleteRobot(Robot robot);
+    }
+}
