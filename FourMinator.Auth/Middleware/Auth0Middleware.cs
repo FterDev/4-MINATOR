02e56af9f0cc81baa53bebe42ad62fc10bd03b3a
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-05-11 19:56:15 +0200

    Added further checks for token

--- FourMinator.Auth/Middleware/Auth0Middleware.cs
+++ FourMinator.Auth/Middleware/Auth0Middleware.cs
@@ -17,15 +17,35 @@ namespace FourMinator.Auth.Middleware
 
         public async Task Invoke(HttpContext context)
         {
+
+            var error = false;
+
             var authHeader = context.Request.Headers["Authorization"];
             if (authHeader.Count == 0)
             {
                 context.Response.StatusCode = 401;
-                await context.Response.WriteAsync("No Authorization header found");
+                error = true;
+            }
+
+            if (authHeader[0] == null)
+            {
+                context.Response.StatusCode = 401;
+                error = true;
+            }
+
+            if (!authHeader[0].StartsWith("Bearer "))
+            {
+                context.Response.StatusCode = 401;
+                error = true;
+            }
+
+            if (error)
+            {
+                await context.Response.WriteAsync("Invalid token or Auth header!");
                 return;
             }
 
-            var token = authHeader[0].Split(" ")[1];
+            var token = authHeader[0].Substring("Bearer ".Length);
 
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             var response = await _httpClient.GetAsync("https://dev-zs8kctz8n04sgjgm.us.auth0.com/userinfo");
