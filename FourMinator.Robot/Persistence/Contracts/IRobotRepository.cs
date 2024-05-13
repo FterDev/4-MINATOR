638d6c1871d880ca502993eb4ce78da517355074
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-05-13 23:39:37 +0200

    Added Statusupdate

--- FourMinator.Robot/Persistence/Contracts/IRobotRepository.cs
+++ FourMinator.Robot/Persistence/Contracts/IRobotRepository.cs
@@ -8,6 +8,7 @@ namespace FourMinator.RobotServices
         Task CreateRobot(Robot robot);
         Task<ICollection<Robot>> GetAllRobots();
         Task<Robot?> GetRobotById(uint id);
+        Task SetStatusByName(string name, RobotStatus status);
         Task UpdateRobot(Robot robot);
         Task DeleteRobot(Robot robot);
     }
