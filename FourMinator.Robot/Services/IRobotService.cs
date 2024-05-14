ff10bd620fcc57a9761f31e28f4c7c3474191386
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-05-14 14:21:12 +0200

    Updated Robot service implementation

---
+++ FourMinator.Robot/Services/IRobotService.cs
@@ -0,0 +1,20 @@
+﻿using FourMinator.Persistence.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourMinator.RobotServices
+{
+    public interface IRobotService
+    {
+        public ICollection<string> Errors { get; }
+        public Task<bool> CreateRobot(string name, string userEmail, string password, string thumbprint, string publicKey);
+        public Task<bool> UpdateRobot(Robot robot);
+        public Task UpdateRobotStatus(string name, RobotStatus status);
+
+        public Task<IEnumerable<Robot>> GetAllRobots();
+
+    }
+}
