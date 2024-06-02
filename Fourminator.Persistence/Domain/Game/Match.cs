e01065d8c25d2b900b7835dd8174814e1bd9658b
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-06-02 18:13:38 +0200

    Created Match Model

---
+++ Fourminator.Persistence/Domain/Game/Match.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourMinator.Persistence.Domain.Game
+{
+    public class Match
+    {
+        public Guid Id { get; set; }
+        public uint PlayerYellowId { get; set; }
+        public uint PlayerRedId { get; set; }
+        public int? Robot { get; set; }
+        public uint? Winner { get; set; }
+        public Int16 YellowStones { get; set; }
+        public Int16 RedStones { get; set; }
+        public bool Active { get; set; }
+        public Int16 State { get; set; }
+        public Player PlayerYellow { get; set; }
+        public Player PlayerRed { get; set; }
+        public Robot RobotForMatch { get; set; }
+    }
+
+    public enum MatchState
+    {
+        Aborted = -1,
+        Pending = 0,
+        Active = 1,
+        Finished = 2
+    }
+}
