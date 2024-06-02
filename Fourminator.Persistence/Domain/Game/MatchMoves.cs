753571f8fcf90a73c32f85c46c6a9012c5a4941b
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-06-02 18:47:55 +0200

    Added MatchMoveModel

---
+++ Fourminator.Persistence/Domain/Game/MatchMoves.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourMinator.Persistence.Domain.Game
+{
+    public class MatchMoves
+    {
+        public uint Id { get; set; }
+        public Guid MatchId { get; set; }
+        public Int16 MoveNumber { get; set; }
+        public Int16 X { get; set; }
+        public Int16 Y { get; set; }
+        public uint PlayerId { get; set; }
+        public bool Color { get; set; }
+        public bool Skipped { get; set; }
+        public bool Joker { get; set; }
+        public uint MoveTime { get; set; }
+        public DateTime MoveTimestamp { get; set; }
+        public Match Match { get; set; }
+        public Player Player { get; set; }
+    }
+}
