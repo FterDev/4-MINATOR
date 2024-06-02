8d8f5d2057e228e343f027b57c2ea0aecf71e9bb
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-06-02 18:33:49 +0200

    Added IMatchRepo interface

---
+++ FourMinator.Game/Persistence/Contracts/IMatchRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FourMinator.GameServices.Persistence.Contracts
+{
+    public interface IMatchRepository
+    {
+        Task CreateMatch(uint playerYellowId, uint playerRedId);
+        Task UpdateMatchState(Guid matchId, Int16 state);
+        Task UpdateMatchWinner(Guid matchId, uint winnerId);
+        Task UpdateMatchStones(Guid matchId, Int16 yellowStones, Int16 redStones);
+        Task<Match> GetMatchById(Guid matchId);
+        Task<IEnumerable<Match>> GetMatchesByPlayerId(uint playerId);
+    }
+}
