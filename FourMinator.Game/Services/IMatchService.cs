a9b336d0cca87f6b19ca6198103cdcebe757d9bf
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-06-02 19:33:08 +0200

    Added MatchService

---
+++ FourMinator.Game/Services/IMatchService.cs
@@ -0,0 +1,19 @@
+﻿using FourMinator.Persistence.Domain.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourMinator.GameServices.Services
+{
+    public interface IMatchService
+    {
+        Task<Match> CreateMatch(uint player1, uint player2);
+        Task UpdateMatchState(Guid matchId, MatchState state);
+        Task UpdateMatchWinner(Guid matchId, uint winnerId);
+        Task UpdateMatchStones(Guid matchId, Int16 yellowStones, Int16 redStones);
+        Task<Match> GetMatchById(Guid matchId);
+        Task<IEnumerable<Match>> GetMatchesByPlayerId(uint playerId);
+    }
+}
