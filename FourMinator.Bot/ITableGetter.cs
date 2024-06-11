25d1084ff8979e1487ef810c4c4c43178fd6ed7f
Dashnor Shala <dashnor_s15@hotmail.com>
2024-06-11 18:28:57 +0200

    Add BotLogic

---
+++ FourMinator.Bot/ITableGetter.cs
@@ -0,0 +1,13 @@
+﻿namespace FourMinator.BotLogic
+{
+    public interface ITableGetter<TKey, TValue>
+    {
+        TKey[] GetKeys();
+        TValue[] GetValues();
+        int GetSize();
+        int GetKeySize();
+        int GetValueSize();
+        TValue Get(TKey key);
+        TValue GetBook(TKey key);
+    }
+}
---
+++ FourMinator.Bot/MoveSorter.cs
@@ -0,0 +1,34 @@
+﻿namespace FourMinator.BotLogic
+{
+    public class MoveSorter
+    {
+        private int size;
+        private readonly (ulong move, int score)[] entries = new (ulong move, int score)[Position.WIDTH];
+
+        public MoveSorter()
+        {
+            size = 0;
+        }
+
+        public void Add(ulong move, int score)
+        {
+            int pos = size++;
+            for (; pos > 0 && entries[pos - 1].score > score; --pos)
+            {
+                entries[pos] = entries[pos - 1];
+            }
+            entries[pos].move = move;
+            entries[pos].score = score;
+        }
+
+        public ulong GetNext()
+        {
+            return size > 0 ? entries[--size].move : 0;
+        }
+
+        public void Reset()
+        {
+            size = 0;
+        }
+    }
+}
