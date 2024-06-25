0554e0cecb62a04531a79cbbda35e489216c4666
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-06-25 13:38:05 +0200

    Edited Robots entity to match EMQX requirements

--- Fourminator.Persistence/Domain/Robot/Robot.cs
+++ Fourminator.Persistence/Domain/Robot/Robot.cs
@@ -2,16 +2,21 @@
 
 
 using FourMinator.Persistence.Domain.Game;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FourMinator.Persistence.Domain
 {
     public class Robot
     {
         public uint Id { get; set; }
+        [Column("username")]
         public string Name { get; set; }
         public int CreatedBy { get; set; }
+
+        [Column("password_hash")]
         public string? Password { get; set; }
-        public string? Thumbprint { get; set; }
+        [Column("salt")]
+        public string? Salt { get; set; }
         public string? PublicKey { get; set; }
         public Int16 Status { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
